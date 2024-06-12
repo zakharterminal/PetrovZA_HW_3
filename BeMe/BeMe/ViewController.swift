e0884ee2e50d7e0772764fe520ef885eff3f37e1
zakhar terminal <zakharterminal@MacBook-Pro-zakhar.local>
2024-06-12 11:38:58 +0300

    init(ByMe)

---
+++ BeMe/BeMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeMe
+//
+//  Created by zakhar terminal on 12.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
